9a5a680761c9ef8fd28e8a9fac72d37371bfb7f1
Jason Hsieh <jasonh117@hotmail.com>
2018-01-16 17:08:10 -0800

    add tlv database, migration, and library functions

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const Database = require('better-sqlite3');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const db = new Database('./db/dev.db');
+const res = db.prepare('SELECT count(*) as count from tlv').get();
+if (res.count <= 0) {
+  const insert = db.prepare('INSERT INTO tlv (tag) VALUES ($tag)');
+  for (let i = 0xE1; i <= 0xEF; i++) {
+    for (let j = 0x01; j <= 0x7F; j++) {
+      insert.run({
+        tag: Buffer.from([0xDF, i, j])
+      });
+    }
+  }
+}
+db.close();
+console.log('Database OK');
 
-var app = express();
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,7 +43,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
---
+++ migrations/20180116172524-tlv.js
@@ -0,0 +1,42 @@
+'use strict';
+
+let dbm;
+let type;
+let seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  return db.createTable('tlv', {
+    tag: { type: 'blob', primaryKey: true },
+    name: { type: 'string' },
+    format: { type: 'string' },
+    length: { type: 'int' },
+    description: { type: 'string' },
+    comments: { type: 'string' },
+    defaultValue: { type: 'string' },
+    detailed: { type: 'string' },
+    tagOwner: { type: 'string' },
+    isPublic: { type: 'boolean' },
+    createdAt: { type: 'datetime', defaultValue: Date.now() },
+    modifiedAt: { type: 'datetime', defaultValue: Date.now() },
+    timesRequested: { type: 'int', defaultValue: 0 },
+    exist: { type: 'boolean', defaultValue: 0 }
+  });
+};
+
+exports.down = function (db) {
+  return db.dropTable('tlv');
+};
+
+exports._meta = {
+  "version": 1
+};
